5d4cfd7aff591a031c836b5e6fd541ce8c6efb2a
daniel obaze <obazedaniel@gmail.com>
2024-01-19 19:56:31 -0500

    code

--- assignment1.js
+++ assignment1.js
@@ -1,3 +1,14 @@
+
+/*********************************************************************************
+*  WEB700 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: chukwudima daniel obaze Student ID: 147137228 Date: 01/19/2024
+*
+********************************************************************************/ 
+
 console.log('WEB700 - Web Server Simulator');
 
 'use strict';
